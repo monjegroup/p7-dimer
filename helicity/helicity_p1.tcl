c00d2ac4cffb654a5155f1497787c6e3b217e344
Toyin Campbell <150461464+ocampbel@users.noreply.github.com>
2025-04-29 11:12:47 -0400

    Create helicity_p1.tcl

---
+++ helicity/helicity_p1.tcl
@@ -0,0 +1,22 @@
+# read coordinate and trajectory
+mol new minimization_system_no_H2O.gro type gro
+mol addfile prodrun_1us_sk10_no_H2O_centered.xtc type xtc first 1 last -1 step 1 waitfor all
+
+# open file to write data
+set file1 [open "helicity_p1.dat" w]
+
+# define selection and number of frames
+set sel [atomselect top "protein and (residue 0 to 62) and name CA"]
+set nf [molinfo top get numframes]
+
+# loop through each frame, collecting the helicity of the protein
+for { set i 0 } { $i < $nf } { incr i } {
+animate goto $i
+display update ui
+$sel frame $i
+mol ssrecalc top
+set secstruct [$sel get structure]
+puts $file1 "$secstruct"
+}
+
+close $file1
